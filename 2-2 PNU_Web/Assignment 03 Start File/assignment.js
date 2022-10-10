219e15b2e181a9d090ff440a1d6ac97fe7af43e4
Sangjun Lee <tkdwns26@naver.com>
2022-10-10 23:48:41 +0900

    commit

--- 2-2 PNU_Web/Assignment 03 Start File/assignment.js
+++ 2-2 PNU_Web/Assignment 03 Start File/assignment.js
@@ -1,17 +1,37 @@
 window.onload = function(){
     var artThumbsInputs = document.querySelectorAll(".artThumb");
-
+    
     for (var i = 0; i < artThumbsInputs.length; i++) {
         artThumbsInputs[i].addEventListener("mouseover",function(){
-            console.log(artThumbsInputs[i].replace(/^.*\//, ''))
+            var largerimg = document.createElement('span');
+            var img = document.createElement('img');
+            var temp = "";
+            var path = "";
+            var index=this.src.length-1;
+            
+            while (this.src[index] != '/'){
+                temp+=this.src[index]
+                index-=1
+            }
 
+            for (var j=temp.length-1; j>=0; j--){
+                path+=temp[j]
+            }
+
+            img.setAttribute('src', './images/art/'+path)
+            largerimg.appendChild(img)
+            largerimg.style.margin = "1.5em";
+            largerimg.style.padding = "0.5em";
+            largerimg.style.border = "0.1em solid";
+            largerimg.style.borderColor = "black";
+            largerimg.style.backgroundColor = "white";
+            largerimg.style.position = "absolute";
+
+            this.parentNode.appendChild(largerimg)
         });
-    }
-    for (var i = 0; i < artThumbsInputs.length; i++) {
         artThumbsInputs[i].addEventListener("mouseout",function(){
-            this.src="images/art/6.jpg"
+            var largerimg = this.parentElement.getElementsByTagName('span');
+            this.parentNode.removeChild(largerimg[0]);
         });
     }
-    
-
 }
